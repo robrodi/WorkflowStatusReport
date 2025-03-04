a37d38be9bb551769f46aa26096f55aa70267cf6
Rob Rodi <1569969+robrodi@users.noreply.github.com>
2025-03-04 15:56:01 -0800

    removed ts changes

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,27 @@
 import * as core from '@actions/core'
 import { wait } from './wait.js'
-//import { context } from '@actions/github'
-// import { Octokit } from '@octokit/action'
+
 /**
  * The main function for the action.
  *
  * @returns Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  //const days: string = core.getInput('days')
+  try {
+    const ms: string = core.getInput('milliseconds')
 
-  // try {
-  //   const octokit = new Octokit({
-  //     auth: 'YOUR-TOKEN'
-  //   })
-  //   // created=2023-02-10..2023-02-12
-  //   // created=>YYYY-MM-DD
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`)
 
-  //   await octokit.request(
-  //     'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?created={createdQuery}&branch=main',
-  //     {
-  //       owner: 'OWNER',
-  //       repo: 'REPO',
-  //       workflow_id: 'WORKFLOW_ID',
-  //       headers: {
-  //         'X-GitHub-Api-Version': '2022-11-28'
-  //       }
-  //     }
-  //   )
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
-  const ms: string = core.getInput('milliseconds')
-
-  // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-  core.debug(`Waiting ${ms} milliseconds ...`)
-
-  // Log the current timestamp, wait, then log the new timestamp
-  core.debug(new Date().toTimeString())
-  await wait(parseInt(ms, 10))
-  core.debug(new Date().toTimeString())
-
-  // Set outputs for other workflow steps to use
-  core.setOutput('time', new Date().toTimeString())
-  // } catch (error) {
-  //   // Fail the workflow run if an error occurs
-  //   if (error instanceof Error) core.setFailed(error.message)
-  // }
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }

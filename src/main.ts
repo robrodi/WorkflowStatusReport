4ab9ca51a2dd1ca85b123d2a1cba781eec485050
Rob Rodi <1569969+robrodi@users.noreply.github.com>
2025-03-04 15:53:05 -0800

    ugh

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import { wait } from './wait.js'
 //import { context } from '@actions/github'
-import { Octokit } from '@octokit/action'
+// import { Octokit } from '@octokit/action'
 /**
  * The main function for the action.
  *
@@ -10,39 +10,39 @@ import { Octokit } from '@octokit/action'
 export async function run(): Promise<void> {
   //const days: string = core.getInput('days')
 
-  try {
-    const octokit = new Octokit({
-      auth: 'YOUR-TOKEN'
-    })
-    // created=2023-02-10..2023-02-12
-    // created=>YYYY-MM-DD
+  // try {
+  //   const octokit = new Octokit({
+  //     auth: 'YOUR-TOKEN'
+  //   })
+  //   // created=2023-02-10..2023-02-12
+  //   // created=>YYYY-MM-DD
 
-    await octokit.request(
-      'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?created={createdQuery}&branch=main',
-      {
-        owner: 'OWNER',
-        repo: 'REPO',
-        workflow_id: 'WORKFLOW_ID',
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      }
-    )
+  //   await octokit.request(
+  //     'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?created={createdQuery}&branch=main',
+  //     {
+  //       owner: 'OWNER',
+  //       repo: 'REPO',
+  //       workflow_id: 'WORKFLOW_ID',
+  //       headers: {
+  //         'X-GitHub-Api-Version': '2022-11-28'
+  //       }
+  //     }
+  //   )
 
-    const ms: string = core.getInput('milliseconds')
+  const ms: string = core.getInput('milliseconds')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+  core.debug(`Waiting ${ms} milliseconds ...`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  // Log the current timestamp, wait, then log the new timestamp
+  core.debug(new Date().toTimeString())
+  await wait(parseInt(ms, 10))
+  core.debug(new Date().toTimeString())
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  // Set outputs for other workflow steps to use
+  core.setOutput('time', new Date().toTimeString())
+  // } catch (error) {
+  //   // Fail the workflow run if an error occurs
+  //   if (error instanceof Error) core.setFailed(error.message)
+  // }
 }

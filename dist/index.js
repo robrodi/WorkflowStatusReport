2062721cf1da6a7d095f095eb356ade4d742ceed
Rob Rodi <1569969+robrodi@users.noreply.github.com>
2025-03-04 15:58:30 -0800

    prettier!

--- dist/index.js
+++ dist/index.js
@@ -27260,45 +27260,28 @@ async function wait(milliseconds) {
     });
 }
 
-//import { context } from '@actions/github'
-// import { Octokit } from '@octokit/action'
 /**
  * The main function for the action.
  *
  * @returns Resolves when the action is complete.
  */
 async function run() {
-    //const days: string = core.getInput('days')
-    // try {
-    //   const octokit = new Octokit({
-    //     auth: 'YOUR-TOKEN'
-    //   })
-    //   // created=2023-02-10..2023-02-12
-    //   // created=>YYYY-MM-DD
-    //   await octokit.request(
-    //     'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?created={createdQuery}&branch=main',
-    //     {
-    //       owner: 'OWNER',
-    //       repo: 'REPO',
-    //       workflow_id: 'WORKFLOW_ID',
-    //       headers: {
-    //         'X-GitHub-Api-Version': '2022-11-28'
-    //       }
-    //     }
-    //   )
-    const ms = coreExports.getInput('milliseconds');
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    coreExports.debug(`Waiting ${ms} milliseconds ...`);
-    // Log the current timestamp, wait, then log the new timestamp
-    coreExports.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    coreExports.debug(new Date().toTimeString());
-    // Set outputs for other workflow steps to use
-    coreExports.setOutput('time', new Date().toTimeString());
-    // } catch (error) {
-    //   // Fail the workflow run if an error occurs
-    //   if (error instanceof Error) core.setFailed(error.message)
-    // }
+    try {
+        const ms = coreExports.getInput('milliseconds');
+        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+        coreExports.debug(`Waiting ${ms} milliseconds ...`);
+        // Log the current timestamp, wait, then log the new timestamp
+        coreExports.debug(new Date().toTimeString());
+        await wait(parseInt(ms, 10));
+        coreExports.debug(new Date().toTimeString());
+        // Set outputs for other workflow steps to use
+        coreExports.setOutput('time', new Date().toTimeString());
+    }
+    catch (error) {
+        // Fail the workflow run if an error occurs
+        if (error instanceof Error)
+            coreExports.setFailed(error.message);
+    }
 }
 
 /**
